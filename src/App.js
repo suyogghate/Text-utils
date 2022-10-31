cf3b1146e2370409664c884f6f005a2eefa26797
Ghate Suyog <suyogghateminks@gmail.com>
2022-10-31 20:14:44 +0530

    added auto dismissing alert, improving dark mode, added favicons

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import React from "react";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
   const [mode, setMode] = React.useState('light'); // wether dark mode is enabled or not
+  const [alert, setAlert] = React.useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
 
   const toggleMode = () => {
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
 
@@ -22,8 +36,9 @@ function App() {
     <>
       {/* <Navbar title="TextUtils" aboutText='About' /> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>  
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below"mode={mode}/>
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze below"mode={mode}/>
         {/* <About /> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,11 +8,13 @@ export default function TextForm(props) {
     // console.log('Uppercase was clicked' + text);
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to uppercase", "success");
   };
 
   const handleLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lowercase", "success");
   }
 
   const handelOnChange = (e) => {
@@ -24,12 +26,20 @@ export default function TextForm(props) {
     var text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to Clipboard", "success");
   }
 
   const handleCamelCase = (e) => {
     var text = document.getElementById("myBox");
     var newText = text.value;
     setText(newText.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()));
+    props.showAlert("Converted to camelcase", "success");
+  }
+
+  const handleClearText = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text cleared!", "success");
   }
 
   return (
@@ -54,10 +64,11 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-primary my-3 mx-3" onClick={handleCopy}>Copy Text</button>
         <button className="btn btn-primary my-3 mx-3" onClick={handleCamelCase}>Camalize</button>
+        <button className="btn btn-primary my-3 mx-3" onClick={handleClearText}>Clear Text</button>
       </div>
       <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.trim() === '' ? 0 : text.match(/\S+/g).length} words and {text.replace(/\s+/g, '').length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes to read.</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox to preview it here..."}</p>
